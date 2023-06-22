eaf430f9f9ecfd5c03ae0fdf2b044e5812273077
Jmserge29 <Jmserge29@gmail.com>
2023-06-22 00:59:08 -0500

    Actu zod for registe

--- Middlewares/validateRequest.js
+++ Middlewares/validateRequest.js
@@ -5,6 +5,6 @@ export const validateSchema = (schema) => (req, res, next) =>{
         schema.parse(req.body)
         next();
     } catch (error) {
-        return res.status(400).json({error})
+        return res.status(400).json({messageError: error.errors.map(error => error.message)})
     }
 }
